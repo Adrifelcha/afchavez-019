c61aaa0b398ec98f4d89087ec2c0155e1109be6f
Adriana Felisa Chavez de la Pena <afchavez@inee.edu.mx>
2018-06-04 19:09:11 -0500

    Estudiando DIF

---
+++ Analisis/DIF/difR package introduction.R
@@ -0,0 +1,15 @@
+#########################################################
+#####  DIF analysis an the difR package
+#####  Reference:
+#####             Title: A general framework and an R package for the detection of dichotomous differential item functioning 
+#####             Authors: David Magis, Sébastien Meland, Francis Tuerlinckx & Paul De Boeck
+rm(list=ls())
+library("difR", lib.loc="~/R/R-3.5.0/library")   
+
+data("verbal")
+
+Mantel_Haeszen <- difMH(Data=verbal, group="Gender",
+      focal.name=1, purify=TRUE, nrIter=20)
+print(Mantel_Haeszen)
+
+plot(Mantel_Haeszen)

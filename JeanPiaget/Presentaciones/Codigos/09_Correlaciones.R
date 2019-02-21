9a9f9cc5e0ebf94cb4095709d75c107537e9ffa2
Adrifelcha <adrifelcha@gmail.com>
2019-02-20 19:00:29 -0600

    Codigo muestra

--- JeanPiaget/Presentaciones/Codigos/09_Correlaciones.R
+++ JeanPiaget/Presentaciones/Codigos/09_Correlaciones.R
@@ -11,18 +11,14 @@ for(i in 1:6){
 
 
 #Correlación perfecta positiva
-plot(semana_1,semana_1, pch=15, xlim= c(100,135), ylim=c(100,135), col="black", cex=2,
-     xlab="Semana 1", ylab="Semana 2")
-lines(c(0,140),c(0,140),col='black', lty=4,lwd=2) 
-for(i in 1:6){
-  text(semana_1[i], semana_1[i]+1, paste(alumnos[i]), cex=1.2)
-}
+x <- c(2,4,6,8,10,12,14)
+y <- rev(x)
+plot(x,y, pch=15, cex=2, xlab="Prueba de felicidad", ylab="Prueba de depresión", cex.lab=1.5)
+lines(c(2,14),c(14,2), col="black", lty=4, lwd=2)
 
 
-#Correlación perfecta negativa
-plot(-(semana_1),-(semana_1), pch=15, xlim= c(-135,-100), ylim=c(-135,-100), col="black", cex=2,
-     xlab="Semana 1", ylab="Semana 2")
-lines(c(0,-140),c(0,-140),col='black', lty=4,lwd=2) 
-for(i in 1:6){
-  text(-(semana_1[i]), -(semana_1[i]-1), paste(alumnos[i]), cex=1.2)
-}
+#Correlación perfecta nula
+y <- rep(120,7)
+x <- rnorm(7,60,5)
+plot(x,y, pch=15, cex=2, xlab="Peso", ylab="Prueba de inteligencia")
+lines(c(0,100),c(120,120), col="black", lty=4, lwd=2)

173f88dcfbba4f4436726695fd5b7edad0614c95
sandra de la peña <adrifelcha@gmail.com>
2019-03-28 21:38:03 -0600

    Holi

--- JeanPiaget/Actividades/Experimento_Piaget.R
+++ JeanPiaget/Actividades/Experimento_Piaget.R
@@ -1,3 +1,10 @@
-setwd("C:/Users/Alejandro/Desktop/afchavez19/JeanPiaget/Actividades")
+setwd("C:/Users/sandra/Desktop/afchavez-019/JeanPiaget/Actividades")
 Datos <- read.csv("Datos_JP.csv")
+View(Datos)
+
 Right <- c("d","c","b")
+
+Datos$RC1 <- ifelse(Datos$RC1==Right[1], Datos$RC1<-1, Datos$RC1<-0)
+Datos$RC2 <- ifelse(Datos$RC2==Right[2], Datos$RC2<-1, Datos$RC2<-0)
+Datos$RC3 <- ifelse(Datos$RC3==Right[3], Datos$RC3<-1, Datos$RC3<-0)
+View(Datos)

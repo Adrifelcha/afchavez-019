0171aa22f5a9223120038249b41cfa48db197036
Adrifelcha <adrifelcha@gmail.com>
2019-04-29 16:45:53 -0500

    RIMEDIE

---
+++ RIMEDIE/Mat09_2015/Inicial/DINAvsDINO/DINA_DINO_Mat09.R
@@ -0,0 +1,118 @@
+########################################################
+# Estimacion del modelo DINA para la aplicación de la prueba
+# PLANEA ELCE 2015 con las especificaciones del modelo 
+# empleadas por el Dr. Guaner Rojas para la prueba PLANEA 06
+#
+#Se incluye también una versión más "rigida" del modelo
+#############################################################
+rm(list=ls())                       #
+library("CDM")
+setwd("C:/Users/Alejandro/Desktop/afchavez19/RIMEDIE/Mat09_2015/Inicial/DINAvsDINO")
+
+Respuestas <- read.csv("PLANEA.09.2015_IDENT_ITEMS.csv")
+Respuestas$ESCUELA <- NULL
+Respuestas$ID_ENT <- NULL 
+Respuestas$NOM_ENT <- NULL
+Respuestas$ID_SERV <- NULL    #Se eliminan las columnas innecesarias
+Respuestas$SERV <- NULL
+Respuestas$MARGINC <- NULL
+Respuestas$TAM_LOC_SEC <- NULL
+Respuestas$SEXO <- NULL
+Respuestas$EDAD_AC <- NULL
+Respuestas$MES_NAC <- NULL
+Respuestas$EDAD <- NULL
+Respuestas$ï..ALUMNO <- NULL
+
+Qmat <- read.csv("QMatriz_MAT09.csv")       
+
+#Se utiliza la Matriz Q para identificar los reactivos que corresponden a cada Eje
+# para realizar la estimación independiente de cada Eje
+
+Eje1 <- NULL
+Eje2 <- NULL
+Eje3 <- NULL
+e1 <- 0
+e2 <- 0
+e3 <- 0
+
+for(i in 1:nrow(Qmat)){              
+  if(Qmat$Eje[i]=="3 - FEM"){
+    e3 <- e3 + 1
+    Eje3[e3] <- Qmat$Item[i]
+  }else{
+    if(Qmat$Eje[i]=="2 - MI"){
+      e2 <- e2+1
+      Eje2[e2] <- Qmat$Item[i]
+    }else{
+      e1 <- e1+1
+      Eje1[e1] <- Qmat$Item[i]
+    }}}
+Qmat$Item <- NULL
+Qmat$Cod_Ram <- NULL         #Se cancelan las columnas innecesarias de la MatrizQ
+Qmat$Eje <- NULL
+Q_E1 <- Qmat[Eje1,1:12]
+Q_E2 <- Qmat[Eje2,13:22]
+Q_E3 <- Qmat[Eje3,23:30]
+R_E1 <- Respuestas[,Eje1]
+R_E2 <- Respuestas[,Eje2]
+R_E3 <- Respuestas[,Eje3]
+#View(Qmat)
+
+
+###########################################################
+# DINA
+
+#Modelo Rigido
+
+#Modelo Laxo (recomendado por Dr. Guaner)
+dino_1 <- din(R_E1, q.matr = Q_E1, rule = "DINO",
+              conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+dina_1 <- din(R_E1, q.matr = Q_E1, rule = "DINA",
+              conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+write.csv(dino_1$item, "E1SNPA_itemparameters_NO.csv")
+write.csv(dino_1$skill.patt, "E1SNPA_skilpatterns_NO.csv")
+write.csv(dino.frame(DINA_1$pattern), "E1SNPA_postpattern_NO.csv",row.names = F)
+
+write.csv(dina_1$item, "E1SNPA_itempars_NA.csv")
+write.csv(dina_1$skill.patt, "E1SNPA_skilpatt_NA.csv")
+write.csv(data.frame(dina_1$pattern), "E1SNPA_fempostpattern_NA.csv",row.names = F)
+
+
+
+#Modelo Rigido para el Eje 2
+dino_2 <- din(R_E2, q.matr = Q_E2, rule = "DINO",
+          conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+#Modelo Laxo
+dina_2 <- din(R_E2, q.matr = Q_E2, rule = "DINA",
+          conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+write.csv(dino_2$item, "E2MI_itemparameters_NO.csv")
+write.csv(dino_2$skill.patt, "E2MI_skilpatterns_NO.csv")
+write.csv(data.frame(dino_2$pattern), "E2MI_postpattern_NO.csv",row.names = F)
+
+write.csv(dina_2$item, "E2MI_itempars_NA.csv")
+write.csv(dina_2$skill.patt, "E2MI_skilpatt_NA.csv")
+write.csv(data.frame(dina_2$pattern), "E2MI_minpostpattern_NA.csv", row.names = F)
+
+
+
+#Modelo Rigido para el Eje 3
+dino_3 <- din(R_E3, q.matr = Q_E3, rule = "DINO",
+          conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+dina_3 <- din(R_E3, q.matr = Q_E3, rule = "DINA",
+          conv.crit = 0.01, maxit = 500, progress = TRUE)
+
+write.csv(dino_3$item, "E3FEM_itemparameters_NO.csv")
+write.csv(dino_3$skill.patt, "E3FEM_skilpatterns_NO.csv")
+write.csv(data.frame(dino_3$pattern), "E3FEM_postpattern_NO.csv",row.names = F)
+
+
+write.csv(dina_3$item, "E3FEM_itempars_NA.csv")
+write.csv(dina_3$skill.patt, "E3FEM_skilpatt_NA.csv")
+write.csv(data.frame(dina_3$pattern), "E3FEM_spapostpattern_NA.csv",row.names = F)
+
+##########################
---
+++ RIMEDIE/Mat09_2015/Post-Reunión25042018/Conf_SinReactivos/0_Confiabilidad_EstructuraInterna-INICIAL.R
@@ -0,0 +1,118 @@
+#############################################################
+# Estimacion de algunos de los índices más importantes de la
+# TCT y Análisis Facoriales Exploratoris y Confirmatorios para 
+# la base de datos empleada para la estimación PLANEA 09 
+#############################################################
+rm(list=ls())
+library(psych)
+setwd("C:/Users/Alejandro/Desktop/afchavez19/RIMEDIE/Mat09_2015/Post-Reunión25042018")
+Respuestas <- read.csv("PLANEA.09.2015_IDENT_ITEMS.csv")
+Respuestas[,1:12] <- NULL
+
+
+Eje1 <- c(c(1:10),c(26:35))           #Items en el Eje 1: SNPA
+Eje2 <- c(c(19:25),43,c(45:50))       #Items en el Eje 2: MI
+Eje3 <- c(c(11:18),c(36:42),44)       #Items en el Eje 3: FEM
+R_E1 <- Respuestas[,Eje1]
+R_E2 <- Respuestas[,Eje2]
+R_E3 <- Respuestas[,Eje3]
+
+#Eliminamos los casos con datos faltantes
+Datos <- Respuestas[complete.cases(Respuestas),]
+D_E1 <- R_E1[complete.cases(R_E1),]
+D_E2 <- R_E2[complete.cases(R_E2),]
+D_E3 <- R_E3[complete.cases(R_E3),]
+
+
+#############################################
+#    ALFA DE CRONBACH      ##################
+#############################################
+#Calculamos las alphas
+Cronbach_general <- alpha(Datos); Cronbach_general    
+# 0.82
+Cronbach_Eje1 <- alpha(D_E1); Cronbach_Eje1        
+# 0.67
+Cronbach_Eje2 <- alpha(D_E2); Cronbach_Eje2
+# 0.59
+Cronbach_Eje3 <- alpha(D_E3); Cronbach_Eje3
+# 0.54
+
+##########################################################
+##########################################################
+##########################################################
+# Primera revisión de la composición de los datos
+# Análisis de Componentes Principales (ACP)
+# Análisis Paralelo
+#############################################
+ACP <- prcomp(Datos, cor=TRUE)
+summary(ACP)
+plot(ACP,type="lines", main="Principal Component Analysis - Screeplot")   #Line Screeplot
+screeplot(ACP)   #Bar screeplot
+#According to the Screeplot, 1 component seems to be good enough
+
+AP <- fa.parallel(Datos); AP
+#Suggest that number of factors = 18 with 5 components
+
+
+#############################################
+# Análisis Factorial 
+#############################################
+Eje_1_1 <- factanal(D_E1, factors = 1, method = "mle") ; Eje_1_1 #SMB09
+Eje_1_2 <- factanal(D_E1, factors = 2, method = "mle") ; Eje_1_2
+Eje_1_3 <- factanal(D_E1, factors = 3, method = "mle") ; Eje_1_3
+
+Eje_2_1 <- factanal(D_E2, factors = 1, method = "mle") ; Eje_2_1 #SMB22 y 24
+Eje_2_2 <- factanal(D_E2, factors = 2, method = "mle") ; Eje_2_2
+Eje_2_3 <- factanal(D_E2, factors = 3, method = "mle") ; Eje_2_3
+
+Eje_3_1 <- factanal(D_E3, factors = 1, method = "mle") ; Eje_3_1 #SMB17 y 15
+Eje_3_2 <- factanal(D_E3, factors = 2, method = "mle") ; Eje_3_2
+Eje_3_3 <- factanal(D_E3, factors = 3, method = "mle") ; Eje_3_3
+
+
+#############################################################
+# Ajustamos y quitamos los reactivos que cargaron bajo para 
+#############################################################
+Respuestas$SMB09 <- NULL
+Respuestas$SMB22 <- NULL
+Respuestas$SMB24 <- NULL
+Respuestas$SMB17 <- NULL
+Respuestas$SMB15 <- NULL
+write.csv(Respuestas, "PLANEA.09.2015_IDENT_ITEMS_ajustado.csv")
+
+
+
+Eje1 <- c(c(1:10),c(26:34))           #Items en el Eje 1: SNPA
+Eje2 <- c(c(19:25),40,c(42:45))       #Items en el Eje 2: MI
+Eje3 <- c(c(11:18),c(35:39),41)       #Items en el Eje 3: FEM
+R_E1 <- Respuestas[,Eje1]
+R_E2 <- Respuestas[,Eje2]
+R_E3 <- Respuestas[,Eje3]
+
+#Eliminamos los casos con datos faltantes
+Datos <- Respuestas[complete.cases(Respuestas),]
+D_E1 <- R_E1[complete.cases(R_E1),]
+D_E2 <- R_E2[complete.cases(R_E2),]
+D_E3 <- R_E3[complete.cases(R_E3),]
+
+
+#############################################
+#    ALFA DE CRONBACH      ##################
+#############################################
+#Calculamos las alphas
+Cronbach_general <- alpha(Datos); Cronbach_general    
+# 0.82
+Cronbach_Eje1 <- alpha(D_E1); Cronbach_Eje1        
+# 0.67
+Cronbach_Eje2 <- alpha(D_E2); Cronbach_Eje2
+# 0.6
+Cronbach_Eje3 <- alpha(D_E3); Cronbach_Eje3
+# 0.55
+
+#############################################
+# Análisis Factorial 
+#############################################
+Eje_1_Aj <- factanal(D_E1, factors = 1, method = "mle") ; Eje_1_Aj #SMB09
+Eje_2_Aj <- factanal(D_E2, factors = 1, method = "mle") ; Eje_2_Aj #SMB22 y 24
+Eje_3_Aj <- factanal(D_E3, factors = 1, method = "mle") ; Eje_3_Aj #SMB17 y 15
+

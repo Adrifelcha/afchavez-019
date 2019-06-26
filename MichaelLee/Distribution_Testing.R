a361ba074cc7de4f00c11e527fae419cef22b8eb
Adrifelcha <adrifelcha@gmail.com>
2019-06-26 14:42:57 -0500

    Intentando nuevos modelos

--- MichaelLee/Distribution_Testing.R
+++ MichaelLee/Distribution_Testing.R
@@ -1,7 +1,7 @@
 
 # 
 #distrib <- rgamma(1000000,1,11)
-distrib <- rnorm(1000000,1.5,1)
+distrib <- rnorm(1000000,0.8,0.5)
 #distrib <- rexp(1000000,)
 plot(density(distrib))
 #lines(c(0,0),c(0,10))
@@ -12,3 +12,17 @@ plot(density(distrib))
 lambdad_B <- rgamma(1000000,.001,.001)
 sigmac_A <- 1/sqrt(lambdad_B)
 plot(density(sigmac_A), xlim=c(0,5))
+
+
+
+
+
+
+density <- ddirichlet(c(.1,.2,.7), c(1,1,1))
+draws <- rdirichlet(20, c(1,1,1) )
+
+library(extraDistr)
+unif_dis <- rdunif(1000000,1,3)
+plot(density(unif_dis), xlim=c(0,5))
+
+

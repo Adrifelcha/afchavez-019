1e254217b4db44e53c39d0a6a76b6979438954f1
Adrifelcha <adrifelcha@gmail.com>
2019-07-02 10:48:37 -0500

    Thought I had already commited

--- MichaelLee/Distribution_Testing.R
+++ MichaelLee/Distribution_Testing.R
@@ -1,7 +1,7 @@
 
 # 
 #distrib <- rgamma(1000000,1,11)
-distrib <- rnorm(1000000,0.8,0.5)
+distrib <- rnorm(1000000,0.95,0.1)
 #distrib <- rexp(1000000,)
 plot(density(distrib))
 #lines(c(0,0),c(0,10))
